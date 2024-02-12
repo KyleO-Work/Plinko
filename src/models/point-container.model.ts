34a686a9f16101cf7fbc104cc7717d6495307c7e
KyleO-Work <koosthuizen.work@gmail.com>
2024-02-12 18:37:19 +0200

    Added javadocs to each method and added the point container text colour to change based on the expected point container to win

--- src/models/point-container.model.ts
+++ src/models/point-container.model.ts
@@ -7,13 +7,15 @@ export class PointContainer {
     endYPos: number;
     value: number;
     renderObject: PIXI.Graphics;
+    textRenderObject: PIXI.Text;
 
-    constructor(startXPos: number, endXPos: number, startYPos: number, endYPos: number, value: number, renderObject: PIXI.Graphics) {
+    constructor(startXPos: number, endXPos: number, startYPos: number, endYPos: number, value: number, renderObject: PIXI.Graphics, textRenderObject: PIXI.Text) {
         this.startXPos = startXPos;
         this.endXPos = endXPos;
         this.startYPos = startYPos;
         this.endYPos = endYPos;
         this.value = value;
         this.renderObject = renderObject;
+        this.textRenderObject = textRenderObject;
     }
 }
